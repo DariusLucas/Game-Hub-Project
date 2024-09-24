af729d59096e25885f8fd059fff20853bcbd3154
DariusLucas <dariusbalogh0@gmail.com>
2024-09-24 16:03:30 +0300

    Improved the color mode switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,16 +1,26 @@
-import { Button, HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Switch, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
-    <HStack padding="10px">
+    <HStack
+      padding="13px"
+      background={colorMode === "dark" ? "#202020" : "#ededed"}
+      borderRadius={30}
+      fontSize="17px"
+    >
       <Switch
-        colorScheme="blue"
+        size="sm"
+        colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
-      />
-      <Text whiteSpace="nowrap">Dark Mode</Text>
+        whiteSpace="nowrap"
+        cursor="pointer"
+        fontFamily="cursive"
+      >
+        Dark Mode
+      </Switch>
     </HStack>
   );
 };
